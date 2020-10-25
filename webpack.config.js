87ef4d9ae2f7a2ba4cf566f2a2db98cd4406198a
Derek Farr <dfarr3@gmail.com>
2020-10-25 19:06:12 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000
+  }
+};
